2549bc59a4ac9847ebd6d279d8697620dc5dc505
Sixten Leemets <s.leemets@gmail.com>
2020-03-23 11:44:51 +0200

    New base proj using express generator

--- client/src/main.js
+++ client/src/main.js
@@ -1,7 +1,21 @@
+// var express = require('express')
+// var cors = require('cors')
+// var app = express()
+// const port = 3000
+
+// app.use(cors())
+
+// app.get('/', (req, res) => res.send('Hello World!'))
+
+// app.listen(port, function () {
+//   console.log('CORS-enabled web server listening on port 80')
+// })
+
+
 import Vue from "vue";
 import App from "./App.vue";
 import router from "./router";
-require('dotenv').config();
+// require('dotenv').config();
 var tools = require('./tools');
 
 Vue.config.productionTip = false;
@@ -11,6 +25,5 @@ new Vue({
   render: h => h(App)
 }).$mount("#app");
 
-console.log(`Key = ${process.env.COINMARKET_API_KEY}`);
-
-tools.importCryptoData(process.env.COINMARKET_API_KEY);
+// console.log(`Key = ${process.env.COINMARKET_API_KEY}`);
+tools.importCryptoData();
